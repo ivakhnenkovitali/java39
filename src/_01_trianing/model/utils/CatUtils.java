e825da007ab759b38b25c9bd5eb9ac1816ac03dc
Asus <vaxa2320@gmail.com>
2024-11-19 17:19:32 +0300

    java_39

--- src/_01_trianing/model/utils/CatUtils.java
+++ src/_01_trianing/model/utils/CatUtils.java
@@ -3,6 +3,9 @@ package _01_trianing.model.utils;
 public class CatUtils {
     public static final String PATH_TO_FILE = "";
 
+    public static void main(String[] args) {
+
+    }
 
 }
 ///•−−• •−• −•−− −•− •−•• •− −•• ••• •−−• −•−− −• • −• −•−−
